cc8739e6cbc211b25eb7af33b1fcfc7e05c0b8ab
Tim Krause <github@tkrause.dev>
2022-04-04 10:47:48 +0200

    E6 - Docker in container (#13)
    Docker + Kubernetes

--- products/build.gradle.kts
+++ products/build.gradle.kts
@@ -1,24 +1,98 @@
+import org.apache.tools.ant.taskdefs.condition.Os
+
 plugins {
     id("org.springframework.boot") version "2.6.5"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    application
     java
 }
 
 group = "de.baleipzig"
 version = "0.0.1-SNAPSHOT"
 
+val imageName = "products"
+val registryIP = "localhost:5001"
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-jersey")
-    implementation ("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
 }
-tasks.compileJava{
+
+application {
+    mainClass.set("de.baleipzig.products.ProductsApplication")
+}
+
+/**
+ * Baut den eigentlichen Docker-Container
+ */
+val buildContainer = tasks.register<Exec>("buildContainer") {
+    group = "internal"
+    dependsOn("bootJar")
+
+    executable = "docker"
+    args("build", "-t", "$imageName:$version", ".")
+}
+
+/**
+ * Pusht den gebauten Docker-Container mit dem Tag "latest"
+ */
+val pushLatest = tasks.register<Exec>("pushLatest") {
+    group = "internal"
+    dependsOn(buildContainer)
+
+    executable = "docker"
+    args("tag", "$imageName:$version", "$registryIP/$imageName:latest")
+
+    doLast {
+        exec {
+            executable = "docker"
+            args("push", "$registryIP/$imageName:latest")
+        }
+    }
+}
+
+/**
+ * Pusht die neu gebaute Version in das Cluster
+ */
+tasks.register<Exec>("redeployProducts") {
+    group = "redeployment"
+    dependsOn(":downloadKubectl", pushLatest)
+
+    val kubeCtl = rootProject.file("build/download/kubectl").absolutePath
+
+    executable = kubeCtl
+    if (Os.isFamily(Os.FAMILY_WINDOWS)) {
+        executable = "$executable.exe"
+    }
+
+    args("rollout", "restart", "deployment/products-deploy")
+
+    doLast {
+        exec {
+            executable = kubeCtl
+            args("rollout", "status", "deployment/products-deploy")
+        }
+    }
+}
+
+tasks.bootJar {
+    mainClass.set("de.baleipzig.products.ProductsApplication")
+}
+
+tasks.register("publishContainer") {
+    group = "internal"
+    dependsOn("clean", pushLatest)
+}
+
+
+tasks.compileJava {
     sourceCompatibility = "17"
     targetCompatibility = "17"
 }
--- products/src/main/java/de/baleipzig/products/ProductsApplication.java
+++ products/src/main/java/de/baleipzig/products/ProductsApplication.java
@@ -1,13 +1,16 @@
 package de.baleipzig.products;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProductsApplication.class, args);
+        final SpringApplication application = new SpringApplication(ProductsApplication.class);
+        application.setWebApplicationType(WebApplicationType.REACTIVE);
+        application.run(args);
     }
 
 }
