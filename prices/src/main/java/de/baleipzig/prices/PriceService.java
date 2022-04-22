636b514e370c80a054cc0de4d8916921a399f6a6
Lisa7030 <Lisa_meschke@live.de>
2022-04-22 10:50:22 +0200

    Anpassungen

--- prices/src/main/java/de/baleipzig/prices/PriceService.java
+++ prices/src/main/java/de/baleipzig/prices/PriceService.java
@@ -7,7 +7,7 @@ import de.baleipzig.prices.persistance.discountPrice.DiscountPriceRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PriceService {
+public class PriceService implements ProduktserviceInterface {
 
     private final DiscountPriceRepository dpRepository;
     private final BasicPriceRepository bpRepository;
@@ -18,60 +18,61 @@ public class PriceService {
         this.bpRepository = bpRepository;
     }
 
-    private Long saveDiscountPrice ( DiscountPrice discountPrice )
-    {
+
+    public Long saveDiscountPrice(DiscountPrice discountPrice) {
         DiscountPrice saveDiscountPrice = dpRepository.save(discountPrice);
         return saveDiscountPrice.getId();
     }
 
-    private Long saveBasicPrice ( BasicPrice basicPrice )
-    {
+
+    public Long saveBasicPrice(BasicPrice basicPrice) {
         BasicPrice saveBasicPrice = bpRepository.save(basicPrice);
         return saveBasicPrice.getId();
     }
 
-    private DiscountPrice getOneDiscountPrice (Long id ){
 
-        return dpRepository.findById(id).orElseThrow(()-> new PriceIdNotFoundException(id));
+    public DiscountPrice getOneDiscountPrice(Long id) {
+
+        return dpRepository.findById(id).orElseThrow(() -> new PriceIdNotFoundException(id));
     }
 
-    private BasicPrice getOneBasicPrice (Long id ){
-        return bpRepository.findById(id).orElseThrow(()-> new PriceIdNotFoundException(id));
+    public BasicPrice getOneBasicPrice(Long id) {
+        return bpRepository.findById(id).orElseThrow(() -> new PriceIdNotFoundException(id));
     }
 
-    private void deleteDiscountPrice ( Long id ) {
+    private void deleteDiscountPrice(Long id) {
 
-        if ( dpRepository.findById(id).isEmpty() ){
+        if (dpRepository.findById(id).isEmpty()) {
             dpRepository.deleteById(id);
-        }else {
+        } else {
             throw new PriceIdNotFoundException(id);
         }
     }
 
-    private void deleteBasicPrice ( Long id ){
+    private void deleteBasicPrice(Long id) {
 
-        if ( bpRepository.findById(id).isEmpty() ){
+        if (bpRepository.findById(id).isEmpty()) {
             bpRepository.deleteById(id);
-        }else {
+        } else {
             throw new PriceIdNotFoundException(id);
         }
     }
 
-    private void deleteDiscountAndBasicPrice ( Long id ){
+    private void deleteDiscountAndBasicPrice(Long id) {
 
-        if ( bpRepository.findById(id).isEmpty() ){
+        if (bpRepository.findById(id).isEmpty()) {
 
-            if ( dpRepository.findById(id).isEmpty() ){
+            if (dpRepository.findById(id).isEmpty()) {
                 dpRepository.deleteById(id);
                 bpRepository.deleteById(id);
-            }else {
-               throw new PriceIdNotFoundException(id);
+            } else {
+                throw new PriceIdNotFoundException(id);
             }
-        }
-        else{
+        } else {
             throw new PriceIdNotFoundException(id);
         }
 
     }
+
 
 }
